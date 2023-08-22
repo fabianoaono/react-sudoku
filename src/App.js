eb0b3d1468d2ff199578b1c9611a2ddc7d2940f2
Fabiano Aono <fabianoaono@gmail.com>
2023-08-21 21:01:06 -0300

    Making fixed cells bolder and editable cells hoverable

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 export default function App() {
   const sudokuArray = 
     [
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      [0, 8, 9, 0, 0, 5, 1, 4, 0],
+      [3, 0, 0, 8, 1, 7, 0, 0, 6],
+      [7, 1, 0, 6, 0, 4, 3, 8, 0],
+      [0, 4, 3, 9, 0, 0, 0, 0, 0],
+      [9, 7, 0, 0, 0, 0, 0, 1, 4],
+      [0, 0, 0, 0, 0, 8, 7, 3, 0],
+      [0, 9, 6, 4, 0, 2, 0, 7, 1],
+      [4, 0, 0, 1, 5, 9, 0, 0, 2],
+      [0, 2, 1, 7, 0, 0, 4, 9, 0]
+    ]
+
+  const sudokuSolution = 
+    [
+      [6, 8, 9, 3, 2, 5, 1, 4, 7],
+      [3, 5, 4, 8, 1, 7, 9, 2, 6],
+      [7, 1, 2, 6, 9, 4, 3, 8, 5],
+      [2, 4, 3, 9, 7, 1, 6, 5, 8],
+      [9, 7, 8, 5, 6, 3, 2, 1, 4],
+      [1, 6, 5, 2, 4, 8, 7, 3, 9],
+      [8, 9, 6, 4, 3, 2, 5, 7, 1],
+      [4, 3, 7, 1, 5, 9, 8, 6, 2],
+      [5, 2, 1, 7, 8, 6, 4, 9, 3]
     ]
 
   const sudokuNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-  const sudokuElements = sudokuArray.map((sudokuRow, rowIndex) => (
+  const sudokuObjectsArray = sudokuArray.map((sudokuRow) => (
+      sudokuRow.map((sudokuCell) => (
+        sudokuCell === 0 ? 
+        {
+          value: "",
+          fixed: true
+        } :
+        {
+          value: sudokuCell,
+          fixed: false
+        }
+      ))
+    )
+  )
+
+  const sudokuElements = sudokuObjectsArray.map((sudokuRow, rowIndex) => (
     <tr key={rowIndex}>
-      {sudokuRow.map((sudokuCell, colIndex) => (
-        <td className="sudoku-cell" key={colIndex}>
-          {sudokuCell}
-        </td>
-      ))}
+      {sudokuRow.map((sudokuCell, colIndex) => {
+        const className = sudokuCell.fixed ? "sudoku-cell" : "sudoku-cell fixed"
+        return (
+          <td className={className} key={colIndex}>
+            {sudokuCell.value}
+          </td>
+        )
+      })}
     </tr>
   ))  
 
