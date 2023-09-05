f7b525374c4e211d3bd175cb9b7dd8711e45b2cb
Fabiano Aono <fabianoaono@gmail.com>
2023-09-04 22:41:52 -0300

    Highlighting same line, row and square

--- src/components/SudokuBoard.js
+++ src/components/SudokuBoard.js
@@ -15,14 +15,44 @@ export default function SudokuBoard(props) {
         )
     })
 
+    const selectedRow = props.selectedCell.row
+    const selectedCol = props.selectedCell.col
+
+    const getClassName = ((sudokuCell, row, col) => {
+        let className = sudokuCell.fixed ? "sudoku-cell fixed" : "sudoku-cell"
+        if (isSelectedCell(row, col)) {
+            className += " selected-cell"
+        } else if (isSameRow(row) || isSameCol(col) || isSameSquare(row, col)) {
+            className += " same-group-cell"
+        }
+        return className
+    })
+
+    const isSameRow = (row => {
+        return row === selectedRow
+    })
+
+    const isSameCol = (col => {
+        return col === selectedCol
+    })
+
+    const isSelectedCell = (row, col) => {
+        return isSameRow(row) && isSameCol(col)
+    }
+
+    const isSameSquare = (row, col) => {
+        const selectedRowSquare = Math.floor(selectedRow / 3)
+        const selectedColSquare = Math.floor(selectedCol / 3)
+        const rowSquare = Math.floor(row / 3)
+        const colSquare = Math.floor(col / 3)
+        return (rowSquare === selectedRowSquare && colSquare === selectedColSquare)
+    }
+
     const sudokuElements = props.sudokuObjectsArray.map((sudokuRow, rowIndex) => (
         <tr key={rowIndex}>
             {sudokuRow.map((sudokuCell, colIndex) => {
-                let className = sudokuCell.fixed ? "sudoku-cell fixed" : "sudoku-cell"
-                if (rowIndex === props.selectedCell.row && colIndex === props.selectedCell.col) {
-                    className += " selected-cell"
-                }
-
+                let className = getClassName(sudokuCell, rowIndex, colIndex)
+                
                 let markedElements = []
                 if (sudokuCell.marked) {
                     markedElements = createMarkedElements(sudokuCell.markValues)
